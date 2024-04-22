19caae5e2335fab35b57d7d91b3a1a84063c914c
wuyeelok <wuyeelok@gmail.com>
2024-04-22 22:26:03 +0800

    Constant & List: Show list of Hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,17 +2,16 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule],
+  imports: [UpperCasePipe, FormsModule, NgFor],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss',
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
 }
