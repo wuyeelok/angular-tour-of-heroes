92140c2758708df93d6333c1f9520a0b9ec34974
wuyeelok <wuyeelok@gmail.com>
2024-04-23 08:23:46 +0800

    Refactor: Extract HeroDetail to component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,11 +4,12 @@ import { UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
 import { NgFor } from '@angular/common';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule, NgFor],
+  imports: [UpperCasePipe, FormsModule, NgFor, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss',
 })
