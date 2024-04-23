620741212cc59e07833ce27f58eb9a9b9b6eb22b
wuyeelok <wuyeelok@gmail.com>
2024-04-23 08:24:02 +0800

    Refactor: Remove unsused import

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { UpperCasePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
 import { NgFor } from '@angular/common';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
@@ -9,7 +7,7 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule, NgFor, HeroDetailComponent],
+  imports: [NgFor, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss',
 })
