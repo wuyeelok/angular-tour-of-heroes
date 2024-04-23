92791ed259b4677a2e0c6e503c0a462a5dcbe4c9
wuyeelok <wuyeelok@gmail.com>
2024-04-23 08:56:19 +0800

    Async: Use RxJS lib to turn getHeroes into Async

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -22,7 +22,7 @@ export class HeroesComponent {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 
   ngOnInit(): void {
